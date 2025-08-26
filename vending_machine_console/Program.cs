702d6e8f61a2ca1b410306e0666e093450372b91
theod <theod@TPC_P1>
2025-08-26 11:53:41 +0200

    Add project files.

---
+++ vending_machine_console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace vending_machine_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
